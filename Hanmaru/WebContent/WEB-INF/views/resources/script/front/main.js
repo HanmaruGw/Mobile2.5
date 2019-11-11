5b6ee0eca19c1397109f90e61e1cfbe2d1b29e1b
jungho <jungho@LAPTOP-94MN4FIB>
2019-11-11 10:00:25 +0900

    2019.11.11 download 경로 변경.

--- Hanmaru/WebContent/WEB-INF/views/resources/script/front/main.js
+++ Hanmaru/WebContent/WEB-INF/views/resources/script/front/main.js
@@ -1,13 +1,13 @@
 $(function(){
-	var android_domain = 'https://dev1.exs-mobile.com:38080/download';
-	var ios_domain = 'https:///www.exs-mobile.com/wp-content/uploads/app';
+	var android_domain = 'https://eptest.halla.com:8080/download';
+	var ios_domain = 'https://eptest.halla.com:8080/download';
 	
 	$('.btn_download.android').click(function(){
 		$(location).attr('href', android_domain+'/hdbsncM-android.apk');
 	});
 	
 	$('.btn_download.ios').click(function(){
-		$(location).attr('href', 'itms-services://?action=download-manifest&url='+ios_domain+'/hdbsncM.plist');
+		$(location).attr('href', 'itms-services://?action=download-manifest&url='+ios_domain+'/hanmaru.plist');
 	});
 });
 
